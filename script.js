11807b78c8dfa409dfb2f97871e935d41fa8a5fb
TaniaPavliuk <106263703+TaniaPavliuk@users.noreply.github.com>
2022-09-14 14:46:50 +0300

    add HW-5

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
-const roundingResult = 0.1 + 0.2
-console.log(roundingResult.toFixed(1));
-
-const userLogin = prompt('Enter your login');
-const userPassword = prompt('Enter your password');
-const login = 'admin';
-const password = '12pass33210';
-if (userLogin !== login && userPassword !== password) {
-    console.log('Wrong login and password!');
-} else if (userLogin !== login) {
-    console.log('Wrong login!');
-} else if (userPassword !== password) {
-    console.log('Wrong password!');
-} else {
-    console.log('Welcome!');
+for (let i = 10; i <= 25; i = i + 0.5) {
+    console.log(i)
 }
 
-const amountOfMoney = +prompt('How much money do you have?');
-console.log('Price-list: watermalon: 21 uah; carrots: 8 uah; potatoes: 12 uah.');
-const watermalon = 21;
-const carrots = 8;
-const potatoes = 12;
-if (amountOfMoney >= Math.min(watermalon, carrots, potatoes)) {
-    const choice = prompt('What would you like to buy?');
-    let productsAmount;
-    let moneyRemainde;
-    switch (choice) {
-        case 'watermalon':
-            productsAmount = amountOfMoney / watermalon;
-            moneyRemainde = amountOfMoney % watermalon;
-            break;
-        case 'carrots':
-            productsAmount = amountOfMoney / carrots;
-            moneyRemainde = amountOfMoney % carrots;
-            break;
-        case 'potatoes':
-            productsAmount = amountOfMoney / potatoes;
-            moneyRemainde = amountOfMoney % potatoes;
-            break;
-        default:
-            console.log('Wrong action!');
+const userInput = prompt('Input the number');
+const userNumber = Number(userInput);
+let isPrime = true;
+for (let i = 2; i < userNumber; i++) {
+    if (userNumber % i === 0) {
+        isPrime = false;
+        break;
     }
+}
+if (isPrime === true) {
+    console.log('It`s a prime number!');
+} else {
+    console.log('It`s not a prime number!');
+}
 
-    if (productsAmount >= 1) {
-        console.log(`Amount: ${Math.floor(productsAmount)} ${choice} Remainder: ${moneyRemainde} uah`);
-    } else {
-        console.log(`Unfortunately you don't have enough money to buy at least one item of ${choice}!`);
+const userCheck = Number(prompt('Please enter the amount of purchase'));
+let discount = 0;
+if (userCheck < 100) {
+    discount = 3;
+} else if (userCheck < 200) {
+    discount = 5;
+} else {
+    discount = 7;
+}
+console.log(`Your discount is ${userCheck / 100 * discount}uah`);
+
+for (let i = 2; i <= 9; i++) {
+    for (let j = 2; j <= 9; j++) {
+        console.log(`${j} * ${i} = `, j * i);
     }
-} else {
-    console.log('Unfortunately you don`t have enough money to buy at least one item!');
 }
 
-const firstSideT = +prompt('Enter the first side of triangle');
-const secondSideT = +prompt('Enter the second side of triangle');
-const thirdSideT = +prompt('Enter the third side of triangle');
-if (firstSideT + secondSideT > thirdSideT && secondSideT + thirdSideT > firstSideT && thirdSideT + firstSideT > secondSideT) {
-    console.log('This triangle is valid!');
-} else {
-    console.log('This triangle is not valid!');
+const rate = 36.76;
+for (let i = 10; i <= 100; i = i + 10) {
+    const currency = i * rate;
+    console.log(`${i} USD = ${currency.toFixed(2)} UAH`);
 }
 
-//2 && 0 && 3 || true && false - false; 
-//false || " " || 3 && true - " ";
-//1 && 1000 && '0' || 0 && 'Bob' - '0';
-//-1 || 0 || 0 && "" || 1010 - -1;
+const numbers = prompt('Please enter fifteen numbers devided by space!');
+const arrayNumbers = numbers.split(' ');
+let positiveNumbers = 0;
+let negativeNumbers = 0;
+let zeroNumbers = 0;
+let evenNumbers = 0;
+let oddNumbers = 0;
+for (let i = 0; i < arrayNumbers.length; i++) {
+    const currentElement = Number(arrayNumbers[i]);
+    if (currentElement > 0) {
+        positiveNumbers++
+    }
+    if (currentElement < 0) {
+        negativeNumbers++
+    }
+    if (currentElement === 0) {
+        zeroNumbers++
+    }
+    if (currentElement % 2 === 0) {
+        evenNumbers++
+    }
+    if (currentElement % 2 !== 0) {
+        oddNumbers++
+    }
+}
+console.log(`There are ${positiveNumbers} positive numbers`);
+console.log(`There are ${negativeNumbers} negative numbers`);
+console.log(`There are ${zeroNumbers} zero numbers`);
+console.log(`There are ${evenNumbers} even numbers`);
+console.log(`There are ${oddNumbers} odd numbers`);
+
+
+
