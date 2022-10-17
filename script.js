d40f94d9a56fce89ae6af0f1bea69b568aff6601
TaniaPavliuk <106263703+TaniaPavliuk@users.noreply.github.com>
2022-10-17 23:54:31 +0300

    add gitignore

--- script.js
+++ script.js
@@ -6,37 +6,40 @@ function Person(name, age, gender, nationality, country, travelCountry) {
   this.country = country;
   this.travelCountry = travelCountry;
 }
-const personOne = new Person("Bohdan", 23, "male", "ukrainian", "Ukraine", [
-  "Greece",
-  "Great Britain",
-  "USA",
+const personOne = new Person('Bohdan', 23, 'male', 'ukrainian', 'Ukraine', [
+  'Greece',
+  'Great Britain',
+  'USA',
 ]);
-const personTwo = new Person("Serhiy", 26, "male", "american", "USA", [
-  "Brazil",
-  "Bosnia",
-  "Serbia",
+const personTwo = new Person('Serhiy', 26, 'male', 'american', 'USA', [
+  'Brazil',
+  'Bosnia',
+  'Serbia',
 ]);
-const personThree = new Person("Vlad", 21, "male", "british", "Great Britain", [
-  "Ukraine",
-  "South Korea",
-  "Canada",
+const personThree = new Person('Vlad', 21, 'male', 'british', 'Great Britain', [
+  'Ukraine',
+  'South Korea',
+  'Canada',
 ]);
-const personFour = new Person("Olexander", 28, "male", "austrian", "Austria", [
-  "Germany",
-  "Netherlands",
-  "France",
+const personFour = new Person('Olexander', 28, 'male', 'austrian', 'Austria', [
+  'Germany',
+  'Netherlands',
+  'France',
 ]);
 console.log(personOne);
-
 
 function acquaintance() {
   console.log(`Hello, dear ${this.name}!`);
 }
 function wakeUp() {
-  console.log(`Wake up ${this.name}! Congratulations! You are not russian. You are ${this.nationality}.`);
+  console.log(
+    `Wake up ${this.name}! Congratulations! You are not russian. You are ${this.nationality}.`
+  );
 }
 function fallAsleep() {
-  console.log(`${this.name}! It's time to fall asleep! It's already night time in ${this.country}!`);
+  console.log(
+    `${this.name}! It's time to fall asleep! It's already night time in ${this.country}!`
+  );
 }
 function travelCountry() {
   console.log(
@@ -52,26 +55,26 @@ travelCountry.call(personFour);
 //
 
 const newPerson = {
-  name: 'Tania'
-}
+  name: 'Tania',
+};
 function info(phone, email) {
-  console.log(`Name: ${this.name}, phone: ${phone}, email: ${email}`)
+  console.log(`Name: ${this.name}, phone: ${phone}, email: ${email}`);
 }
 function bind(fn, context, ...rest) {
   return function (...args) {
-    const uniqId = Date.now().toString() + Math.random
-    context[uniqId] = fn
-    const result = context[uniqId](...rest.concat(args))
-    delete context[uniqId]
-    return result
-  }
-}  
-bind(info, newPerson, '+380635281511', 'v.sugar25@gmail.com')()
-console.log(newPerson)
+    const uniqId = Date.now().toString() + Math.random;
+    context[uniqId] = fn;
+    const result = context[uniqId](...rest.concat(args));
+    delete context[uniqId];
+    return result;
+  };
+}
+bind(info, newPerson, '+380635281511', 'v.sugar25@gmail.com')();
+console.log(newPerson);
 
 //
 
-function isEqual(object1, object2) {
+function isEqual(obj1git, obj2) {
   const info1 = Object.getOwnPropertyNames(obj1);
   const info2 = Object.getOwnPropertyNames(obj2);
   if (info1.length !== info2.length) {
@@ -87,19 +90,18 @@ function isEqual(object1, object2) {
 }
 
 const obj1 = {
-  name: "Tania",
-  surname: "Pavliuk"
+  name: 'Tania',
+  surname: 'Pavliuk',
 };
 const obj2 = {
-  surname: "Pavliuk",
-  name: "Tania"
+  surname: 'Pavliuk',
+  name: 'Tania',
 };
 console.log(isEqual(obj1, obj2));
 
 //
 
-
-// function NOD(A) {   
+// function NOD(A) {
 //     var n = A.length, x = Math.abs(A[0]);
 //     for (var i = 1; i < n; i++)
 //      { var y = Math.abs(A[ i ]);
@@ -109,50 +111,50 @@ console.log(isEqual(obj1, obj2));
 //     return x;
 // }
 
-// function NOK(A) `{   
+// function NOK(A) `{
 //     var  n = A.length, a = Math.abs(A[0]);
 //     for (var i = 1; i < n; i++)
 //      { var b = Math.abs(A[ i ]), c = a;
-//        while (a && b){ a > b ? a %= b : b %= a; } 
+//        while (a && b){ a > b ? a %= b : b %= a; }
 //        a = Math.abs(c*A[ i ])/(a+b);
 //      }
 //     return a;
 // }
-  
+
 //
 
 function gcd(x, y) {
-	if (!y) {
-		return x;
-	}
-	return gcd(y, x % y);
+  if (!y) {
+    return x;
+  }
+  return gcd(y, x % y);
 }
 
 function lcm(x, y) {
-	return x * y / gcd(x, y);
+  return (x * y) / gcd(x, y);
 }
 
 function Calculator() {
-	this.enterData = function () {
-		this.a = parseInt(prompt('Enter a'));
-		this.b = parseInt(prompt('Enter b'));
-	}
+  this.enterData = function () {
+    this.a = parseInt(prompt('Enter a'));
+    this.b = parseInt(prompt('Enter b'));
+  };
 
-	this.calculateSum = function () {
-		return this.a + this.b;
-	}
+  this.calculateSum = function () {
+    return this.a + this.b;
+  };
 
-	this.calculateNSD = function () {
-		return gcd(this.a, this.b);
-	}
+  this.calculateNSD = function () {
+    return gcd(this.a, this.b);
+  };
 
-	this.calculateNSK = function () {
-		return lcm(this.a, this.b);
-	}
+  this.calculateNSK = function () {
+    return lcm(this.a, this.b);
+  };
 }
 
 const calc = new Calculator();
 calc.enterData();
-console.log("Sum: " + calc.calculateSum());
-console.log("NSD: " + calc.calculateNSD());
-console.log("NSK: " + calc.calculateNSK());
+console.log('Sum: ' + calc.calculateSum());
+console.log('NSD: ' + calc.calculateNSD());
+console.log('NSK: ' + calc.calculateNSK());
