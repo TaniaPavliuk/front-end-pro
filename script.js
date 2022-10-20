f1061df5ea47d5b5904e1070da5cc7270197aa10
TaniaPavliuk <106263703+TaniaPavliuk@users.noreply.github.com>
2022-10-21 00:24:28 +0300

    add script

--- script.js
+++ script.js
@@ -1,160 +1,237 @@
-function Person(name, age, gender, nationality, country, travelCountry) {
-  this.name = name;
-  this.age = age;
-  this.gender = gender;
-  this.nationality = nationality;
-  this.country = country;
-  this.travelCountry = travelCountry;
-}
-const personOne = new Person('Bohdan', 23, 'male', 'ukrainian', 'Ukraine', [
-  'Greece',
-  'Great Britain',
-  'USA',
-]);
-const personTwo = new Person('Serhiy', 26, 'male', 'american', 'USA', [
-  'Brazil',
-  'Bosnia',
-  'Serbia',
-]);
-const personThree = new Person('Vlad', 21, 'male', 'british', 'Great Britain', [
-  'Ukraine',
-  'South Korea',
-  'Canada',
-]);
-const personFour = new Person('Olexander', 28, 'male', 'austrian', 'Austria', [
-  'Germany',
-  'Netherlands',
-  'France',
-]);
-console.log(personOne);
+/* eslint-disable max-classes-per-file */
+const MIN_CAPACITY_REDUCTION = 0.1;
+const MAX_CAPACITY_REDUCTION = 2.5;
+const INITIAL_CAPACITY = 100;
 
-function acquaintance() {
-  console.log(`Hello, dear ${this.name}!`);
-}
-function wakeUp() {
-  console.log(
-    `Wake up ${this.name}! Congratulations! You are not russian. You are ${this.nationality}.`
-  );
-}
-function fallAsleep() {
-  console.log(
-    `${this.name}! It's time to fall asleep! It's already night time in ${this.country}!`
-  );
-}
-function travelCountry() {
-  console.log(
-    `Dear ${this.name} this month you should visit these countries: ${this.travelCountry}`
-  );
+function getRandomCapacity() {
+  return (
+    Math.random() * MAX_CAPACITY_REDUCTION +
+    MIN_CAPACITY_REDUCTION
+  ).toFixed(1);
 }
 
-acquaintance.call(personTwo);
-wakeUp.call(personOne);
-fallAsleep.call(personThree);
-travelCountry.call(personFour);
-
-//
-
-const newPerson = {
-  name: 'Tania',
-};
-function info(phone, email) {
-  console.log(`Name: ${this.name}, phone: ${phone}, email: ${email}`);
-}
-function bind(fn, context, ...rest) {
-  return function (...args) {
-    const uniqId = Date.now().toString() + Math.random;
-    context[uniqId] = fn;
-    const result = context[uniqId](...rest.concat(args));
-    delete context[uniqId];
-    return result;
-  };
-}
-bind(info, newPerson, '+380635281511', 'v.sugar25@gmail.com')();
-console.log(newPerson);
-
-//
-
-function isEqual(obj1git, obj2) {
-  const info1 = Object.getOwnPropertyNames(obj1);
-  const info2 = Object.getOwnPropertyNames(obj2);
-  if (info1.length !== info2.length) {
-    return false;
+class Car {
+  constructor(
+    fuelConsumption,
+    engineType,
+    engineVolume,
+    model,
+    yearOfProduction,
+    weight
+  ) {
+    this.fuelConsumption = fuelConsumption;
+    this.engineType = engineType;
+    this.engineVolume = engineVolume;
+    this.model = model;
+    this.yearOfProduction = yearOfProduction;
+    this.weight = weight;
+    this.isAvailable = true;
+    this.capacity = INITIAL_CAPACITY;
   }
-  for (let i = 0; i < info1.length; i += 1) {
-    const info = info1[i];
-    if (obj1[info] !== obj2[info]) {
-      return false;
+
+  startDrive() {
+    if (this.isAvailable === true) {
+      const newCapacity = this.capacity - getRandomCapacity();
+      if (newCapacity > 0) {
+        this.isAvailable = false;
+        this.capacity = newCapacity;
+        console.log(`Let's drive ${this.model}!`);
+      } else {
+        console.log('Car has no capacity!');
+      }
+    } else {
+      console.log('Car is not available!');
     }
   }
-  return true;
-}
 
-const obj1 = {
-  name: 'Tania',
-  surname: 'Pavliuk',
-};
-const obj2 = {
-  surname: 'Pavliuk',
-  name: 'Tania',
-};
-console.log(isEqual(obj1, obj2));
-
-//
-
-// function NOD(A) {
-//     var n = A.length, x = Math.abs(A[0]);
-//     for (var i = 1; i < n; i++)
-//      { var y = Math.abs(A[ i ]);
-//        while (x && y){ x > y ? x %= y : y %= x; }
-//        x += y;
-//      }
-//     return x;
-// }
-
-// function NOK(A) `{
-//     var  n = A.length, a = Math.abs(A[0]);
-//     for (var i = 1; i < n; i++)
-//      { var b = Math.abs(A[ i ]), c = a;
-//        while (a && b){ a > b ? a %= b : b %= a; }
-//        a = Math.abs(c*A[ i ])/(a+b);
-//      }
-//     return a;
-// }
-
-//
-
-function gcd(x, y) {
-  if (!y) {
-    return x;
+  stopDrive() {
+    if (this.isAvailable === false) {
+      this.isAvailable = true;
+      console.log(`Stop Drive ${this.model}!`);
+    }
   }
-  return gcd(y, x % y);
 }
 
-function lcm(x, y) {
-  return (x * y) / gcd(x, y);
+class Wagon extends Car {}
+
+class Sedan extends Car {}
+
+class Hatchback extends Car {}
+
+function getPriceByYear(carYear, vehicleType) {
+  switch (vehicleType) {
+    case 'hatchback':
+      if (carYear >= 2019) {
+        return 1.2;
+      }
+      if (carYear >= 2010 && carYear <= 2018) {
+        return 1.4;
+      }
+      if (carYear >= 2000 && carYear <= 2009) {
+        return 1.7;
+      }
+      if (carYear >= 1990 && carYear <= 1999) {
+        return 2;
+      }
+      break;
+    case 'sedan':
+      if (carYear >= 2019) {
+        return 1.5;
+      }
+      if (carYear >= 2010 && carYear <= 2018) {
+        return 1.8;
+      }
+      if (carYear >= 2000 && carYear <= 2009) {
+        return 2;
+      }
+      if (carYear >= 1990 && carYear <= 1999) {
+        return 2.6;
+      }
+      break;
+    case 'wagon':
+      if (carYear >= 2019) {
+        return 2;
+      }
+      if (carYear >= 2010 && carYear <= 2018) {
+        return 2.2;
+      }
+      if (carYear >= 2000 && carYear <= 2009) {
+        return 2.5;
+      }
+      if (carYear >= 1990 && carYear <= 1999) {
+        return 3;
+      }
+      break;
+    default:
+      console.log('invalid year!');
+  }
 }
 
-function Calculator() {
-  this.enterData = function () {
-    this.a = parseInt(prompt('Enter a'));
-    this.b = parseInt(prompt('Enter b'));
-  };
-
-  this.calculateSum = function () {
-    return this.a + this.b;
-  };
-
-  this.calculateNSD = function () {
-    return gcd(this.a, this.b);
-  };
-
-  this.calculateNSK = function () {
-    return lcm(this.a, this.b);
-  };
+function getPriceByEngineType(engineType, vehicleType) {
+  switch (vehicleType) {
+    case 'hatchback':
+      if (engineType === 'diesel') {
+        return 2.2;
+      }
+      if (engineType === 'petrol') {
+        return 1.8;
+      }
+      break;
+    case 'sedan':
+      if (engineType === 'diesel') {
+        return 2.5;
+      }
+      if (engineType === 'petrol') {
+        return 2;
+      }
+      break;
+    case 'wagon':
+      if (engineType === 'diesel') {
+        return 2.9;
+      }
+      if (engineType === 'petrol') {
+        return 2.4;
+      }
+      break;
+    default:
+      console.log('invalid type of engine!');
+  }
 }
 
-const calc = new Calculator();
-calc.enterData();
-console.log('Sum: ' + calc.calculateSum());
-console.log('NSD: ' + calc.calculateNSD());
-console.log('NSK: ' + calc.calculateNSK());
+function getPriceByWeight(weight, vehicleType) {
+  switch (vehicleType) {
+    case 'hatchback':
+      if (weight >= 800 && weight <= 1100) {
+        return 1.5;
+      }
+      if (weight >= 1101 && weight <= 1400) {
+        return 1.7;
+      }
+      if (weight >= 1401) {
+        return 2;
+      }
+      break;
+    case 'sedan':
+      if (weight >= 800 && weight <= 1100) {
+        return 1.6;
+      }
+      if (weight >= 1101 && weight <= 1400) {
+        return 1.8;
+      }
+      if (weight >= 1401) {
+        return 2.1;
+      }
+      break;
+    case 'wagon':
+      if (weight >= 800 && weight <= 1100) {
+        return 1.7;
+      }
+      if (weight >= 1101 && weight <= 1400) {
+        return 1.9;
+      }
+      if (weight >= 1401) {
+        return 2.2;
+      }
+      break;
+    default:
+      console.log('invalid weight!');
+  }
+}
+
+function service(vehicle) {
+  let priceIndex = 0;
+  let indices = 0;
+  let finalPrice = 0;
+
+  if (vehicle.yearOfProduction < 1990) {
+    console.log('too old');
+    return;
+  }
+
+  if (vehicle.yearOfProduction < 800) {
+    console.log('too light');
+    return;
+  }
+
+  if (vehicle instanceof Wagon) {
+    priceIndex = 130;
+    indices += getPriceByYear(vehicle.yearOfProduction, 'wagon');
+    indices += getPriceByEngineType(vehicle.engineType, 'wagon');
+    indices += getPriceByWeight(vehicle.weight, 'wagon');
+  }
+  if (vehicle instanceof Sedan) {
+    priceIndex = 125;
+    indices += getPriceByYear(vehicle.yearOfProduction, 'sedan');
+    indices += getPriceByEngineType(vehicle.engineType, 'sedan');
+    indices += getPriceByWeight(vehicle.weight, 'sedan');
+  }
+  if (vehicle instanceof Hatchback) {
+    priceIndex = 110;
+    indices += getPriceByYear(vehicle.yearOfProduction, 'hatchback');
+    indices += getPriceByEngineType(vehicle.engineType, 'hatchback');
+    indices += getPriceByWeight(vehicle.weight, 'hatchback');
+  }
+
+  console.log(
+    `car was wrecked by: ${(INITIAL_CAPACITY - vehicle.capacity).toFixed(2)}`
+  );
+
+  finalPrice = indices * priceIndex * 0.1;
+  return finalPrice;
+}
+
+const honda = new Sedan(7.7, 'petrol', 2.0, 'Honda Accord', 2014, 1500);
+honda.startDrive();
+honda.stopDrive();
+
+const priceForHonda = service(honda);
+console.log(priceForHonda);
+
+// const bmw = new Car(9.0, "petrol", 3.0, "BMW 520i", 2018, 1600);
+// bmw.startDrive();
+// bmw.stopDrive();
+// bmw.startDrive();
+// bmw.startDrive();
+// const audi = new Car(8.1, "diesel", 2.5, "Audi Q7", 2019, 3000);
+// audi.startDrive();
+// audi.stopDrive();
